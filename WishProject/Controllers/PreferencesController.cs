31fd6527041dc3b96973838a632f7995e89df2cc
VJS <sigulda@hotmail.com>
2015-04-29 17:29:12 +0100

    version3

---
+++ WishProject/Controllers/PreferencesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WishProject.Models;
+
+namespace WishProject.Controllers
+{
+    public class PreferencesController : Controller
+    {
+        private WishProjectContext db = new WishProjectContext();
+
+        // GET: Preferences
+        public async Task<ActionResult> Index()
+        {
+            var preferences = db.Preferences.Include(p => p.User);
+            return View(await preferences.ToListAsync());
+        }
+
+        // GET: Preferences/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preference preference = await db.Preferences.FindAsync(id);
+            if (preference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preference);
+        }
+
+        // GET: Preferences/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Email");
+            return View();
+        }
+
+        // POST: Preferences/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "PreferenceId,Occasions,UserPreference,Flowers,UserId")] Preference preference)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Preferences.Add(preference);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Email", preference.UserId);
+            return View(preference);
+        }
+
+        // GET: Preferences/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preference preference = await db.Preferences.FindAsync(id);
+            if (preference == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Email", preference.UserId);
+            return View(preference);
+        }
+
+        // POST: Preferences/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "PreferenceId,Occasions,UserPreference,Flowers,UserId")] Preference preference)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(preference).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "Email", preference.UserId);
+            return View(preference);
+        }
+
+        // GET: Preferences/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Preference preference = await db.Preferences.FindAsync(id);
+            if (preference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preference);
+        }
+
+        // POST: Preferences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Preference preference = await db.Preferences.FindAsync(id);
+            db.Preferences.Remove(preference);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WishProject/Models/WishProjectContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WishProject.Models
+{
+    public class WishProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WishProjectContext() : base("name=WishProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WishProject.Models.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<WishProject.Models.Preference> Preferences { get; set; }
+    
+    }
+}
