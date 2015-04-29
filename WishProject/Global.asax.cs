d453eaa6b151cfdd3ea4dcdd61cbec32f5cea3d0
VJS <sigulda@hotmail.com>
2015-04-29 17:38:21 +0100

    version4

--- WishProject/Global.asax.cs
+++ WishProject/Global.asax.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WishProject.Models;
 
 namespace WishProject
 {
@@ -12,6 +13,8 @@ namespace WishProject
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<WishProjectContext>(new WPInitializer()); 
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
